966d98ef4a3770bc61f3bb39c9edb58f6e48fa8e
Anthony Cantellano <AcAntellAno@users.noreply.github.com>
2018-07-08 20:48:49 -0700

    added db schema and test user

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 var request = require('request');
 const _PORT = 8080;
 
+mongoose.connect('mongodb://localhost/tftb');
 app.use(express.static('public')); //serve contents in public directory
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+//Schema
+var userSchema = new mongoose.Schema({
+  fname: String,
+  lname: String,
+  email: String,
+  password: String
+});
+
+var User = mongoose.model('User', userSchema);
+
+User.create({
+  fname: 'Tom',
+  lname: 'Caca',
+  email: 'tcaca@gmail.com',
+  password: 'test'
+});
+
+ app.get('/', (req, res) => {
   res.render('landing');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
 });
 
 app.listen(_PORT, () => {
   console.log('thongzfortheboiz.com server is running...');
 });
-//asdjfajlkdfjalkdjfklajklfjadf
